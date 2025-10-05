18d08cfdfc37efa62a91ca0c8ecab2756e0fb7de
Nidhi5430 <nidhidubey1445@gmail.com>
2025-10-05 16:57:02 +0530

    first upload

--- gallery.php
+++ gallery.php
@@ -3,4 +3,25 @@
 include('header.php');
 
 ?>
+  <!-- why section -->
+      <div class="row">
+      <div class="col-md-3">
 
+      </div>
+      <div class="col-md-3">
+      <div class="card" style="width: 18rem;height:100px;">
+  <img src="images/temple1.jpg" style="height:300px;" class="card-img-top" alt="...">
+ 
+</div>
+      </div>
+      <div class="col-md-3">
+      <div class="card" style="width: 18rem;height:100px;">
+  <img src="images/temple2.jpg" style="height:300px;" class="card-img-top" alt="...">
+ 
+</div>
+          </div>
+          <div class="col-md-3">
+          
+          </div>
+  </div>
+      <!-- end why section -->
